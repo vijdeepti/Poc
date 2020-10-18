cc95f2eaf272a5faf6698a3bffc28ee78d82fe59
Ankit Mishra <ankit.mishra@theredpandas.com>
2020-10-18 19:41:29 +0530

    formatting

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const redis = require("redis");
 // Authenticate!
 io.origins('*:*');
 
-const joinRoom = (userId=1, socket) =>{
+const joinRoom = (userId = 1, socket) => {
   socket.join(userId);
 }
 /**default namespace connection- can be used */
 //_.each(io.nsps, forbidConnections);
-async function verifyUser (userId) {
+async function verifyUser(userId) {
   return new Promise((resolve, reject) => {
     // setTimeout to mock a cache or database call
     setTimeout(() => {
@@ -30,8 +30,8 @@ async function verifyUser (userId) {
         },
       ];
 
-      const user = users.find(user=>user.name === userId)
-      console.log(userId,user)
+      const user = users.find(user => user.name === userId)
+      console.log(userId, user)
       if (!user) {
         return reject('USER_NOT_FOUND');
       }
@@ -41,44 +41,44 @@ async function verifyUser (userId) {
   });
 }
 
-io.on('connection', function(socket){
-  socket.auth = false;
+io.on('connection', function (socket) {
 
-  socket.on('authenticate', function(data){
+  socket.on('authenticate', function (data) {
     console.log('authenticate event')
-    verifyUser(data,socket).then(()=>{
-      socket.auth = true;
-     _.each(io.nsps, function(nsp) {
+    verifyUser(data, socket).then(() => {
+      _.each(io.nsps, function (nsp) {
         restoreConnection(nsp, socket);
       });
-      joinRoom(data,socket)
-     socket.emit('authenticated','')
-    }).catch(()=>{
-      socket.emit('unAuthorised','')
-      socket.disconnect();
+      joinRoom(data, socket)
+      socket.emit('authenticated', '')
+    }).catch(() => {
+      socket.emit('unAuthorised', '')
+      // socket.disconnect();
     })
   });
-  socket.on('disconnect', function(data){
-    socket.auth = false;
+  socket.on('disconnect', function () {
+
   });
-  socket.on('getmessage',function(userId){
-  io.to(userId).emit('message','message is for user '+userId+' '+Date.now());
+
+  socket.on('getmessage', function (userId) {
+    io.to(userId).emit('message', 'message is for user ' + userId + ' ' + Date.now());
   })
 });
 
-function forbidConnections(nsp) {
-  nsp.on('connect', function(socket) {
-    if (!socket.auth) {
-      delete nsp.connected[socket.id];
-    }
-  });
-}
+
+// function forbidConnections(nsp) {
+//   nsp.on('connect', function(socket) {
+//     if (!socket.auth) {
+//       delete nsp.connected[socket.id];
+//     }
+//   });
+// }
 
 /**
  * If the socket attempted a connection before authentication, restore it.
  */
 function restoreConnection(nsp, socket) {
-  if (_.find(nsp.sockets, {id: socket.id})) {
+  if (_.find(nsp.sockets, { id: socket.id })) {
     nsp.connected[socket.id] = socket;
   }
 }
